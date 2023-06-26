55b744974376d495e97914824afc4fb57fef8ac0
Pankaj Singh <pankajsingh1397@gmail.com>
2023-06-26 14:28:55 +0530

    initial commit

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import state from '../store'
+import { useSnapshot } from 'valtio'
+
+
+const CustomButton = ({type, title, customStyles, handleClick}) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if(type ==='filled') {
+      return {
+        backgroundColor : snap.color,
+        color : "#fff",
+      }
+    }
+  }
+
+  return (
+    <button className={`px-2 py-1.2 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
---
+++ src/pages/Customizer.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react' 
+
+const Customizer = () => {
+  return (
+    <div>Customizer</div>
+  )
+}
+
+export default Customizer
