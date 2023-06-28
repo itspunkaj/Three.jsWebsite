70fd1d6afb0d4113fca1af218f55102345507be0
Pankaj Singh <pankajsingh1397@gmail.com>
2023-06-28 23:27:55 +0530

    Add Shirt

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,10 +1,28 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { Environment, Center } from '@react-three/drei'
 
-const Canvas = () => {
+import CameraRig from './CameraRig';
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      {/*eslint-disable-next-line react/no-unknown-property*/}
+      <ambientLight intensity={0.5} />
+      <Environment preset='city' />
+
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel;
