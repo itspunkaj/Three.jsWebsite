2f1e1087ea3569a1d87aa1d027b4f309cf113365
Pankaj Singh <pankajsingh1397@gmail.com>
2023-07-08 23:10:59 +0530

    Logo-Editor Done

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -4,8 +4,10 @@ import { SketchPicker } from 'react-color'
 import { useSnapshot } from 'valtio'
 
 import state from '../store'
+
 const ColorPicker = () => {
   const snap = useSnapshot(state);
+  
   return (
     <div className='absolute left-full ml-3'>
       <SketchPicker 
